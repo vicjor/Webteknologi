3081b2884bb4c5f9c408ce1a75efde5f44d20668
William Østensen <william-98-@hotmail.com>
2018-11-06 12:12:23 +0100

    navbar links

--- mediafy/js/navbar.js
+++ mediafy/js/navbar.js
@@ -1,11 +1,14 @@
 document.getElementById('navbar').innerHTML = '<div> <header id="header"> <div class="navbar"> \
-<h1> <a href="#">MEDIAFY</a></h1>\
+<h1> <a href="index.html">MEDIAFY</a></h1>\
 <nav>\
 <ul>\
-  <li><a href="#">HOME</a> </li>\
-  <li><a href="#">ABOUT</a> </li>\
-  <li><a href="#">MUSIC</a> </li>\
-  <li><a href="#">FILM</a> </li>\
-  <li><a href="#">SERIES</a> </li>\
+  <li><a href="index.html">HOME</a> </li>\
+  <li><a href="about.html">ABOUT</a> </li>\
+  <li><a href="music.html">MUSIC</a> </li>\
+  <li><a href="movies.html">FILM</a> </li>\
+  <li><a href="series.html">SERIES</a> </li>\
+  <li class="icon">\
+    <a href="javascript:void(0);" onclick="myFunction()">&#9776;</a>\
+  </li>\
 </ul>\
 </nav> </div> </header> </div>';
