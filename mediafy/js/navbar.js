b4102d763b4af9058c56fe0ba2bee47c6b1e8b56
Victor Jørgensen <victor.jorgens@gmail.com>
2018-11-06 12:10:12 +0100

    movies and about

---
+++ mediafy/js/navbar.js
@@ -0,0 +1,11 @@
+document.getElementById('navbar').innerHTML = '<div> <header id="header"> <div class="navbar"> \
+<h1> <a href="#">MEDIAFY</a></h1>\
+<nav>\
+<ul>\
+  <li><a href="#">HOME</a> </li>\
+  <li><a href="#">ABOUT</a> </li>\
+  <li><a href="#">MUSIC</a> </li>\
+  <li><a href="#">FILM</a> </li>\
+  <li><a href="#">SERIES</a> </li>\
+</ul>\
+</nav> </div> </header> </div>';
