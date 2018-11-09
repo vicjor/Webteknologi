4bd6af753561698aee1196f14b3b641a726465ac
William Østensen <william-98-@hotmail.com>
2018-11-09 21:53:54 +0100

    slider farger

--- mediafy/js/middleimageslider.js
+++ mediafy/js/middleimageslider.js
@@ -7,7 +7,7 @@ var text3 = document.getElementById('innertext3');
 var columnlist = [column1,column2,column3];
 var textlist = [text1,text2,text3];
 var text = ['MUSIC','MOVIES','SERIES','DISCOVER','NEW RELEASES','EDITORS PICKS','DISCOVER','THE<br>BEST<br>OF','EVERY<br>THING','NEW','CONTENT','EVERY WEEK','WE','ARE','MEDIAFY','THE','LATEST','IN','MUSIC','MOVIES','SERIES'];
-var list = ['#DEB887','#DEB887','#DEB887','#DAA520','#D2B48C','#CD853F','#8B4513','#CD853F','#A0522D','#DAA520','#D2B48C','#8B4513'];
+var list = ['#DEB887','#DEB887','#DEB887','#b89866','#D2B48C','#d3b17d'];
 var index = 0;
 
 
